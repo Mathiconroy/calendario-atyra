02a81a628ff0445889838b4d54c9218a14571333
Mathiconroy <mathiconroy@gmail.com>
2019-12-30 18:01:03 -0300

    Added bootstrap and table html page

--- calendario_atyra_root/calendarios/views.py
+++ calendario_atyra_root/calendarios/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 def index(request):
-    return render(request, 'calendarios/index.html')
+    return render(request, 'calendarios/main.html')
 
 # Create your views here.
