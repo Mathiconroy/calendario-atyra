df606de840f4b8f048428d653f2eb4546679f624
Mathiconroy <mathiconroy@gmail.com>
2020-01-03 16:08:27 -0300

    Trying to save values in db

--- calendario_atyra_root/calendarios/views.py
+++ calendario_atyra_root/calendarios/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from .forms import AddClientForm
+from .models import Reservas
 
 def index(request):
-    return render(request, 'calendarios/main.html')
+    reserva = Reservas.objects.filter()
+    return render(request, 'calendarios/main.html', {'nombre':reserva[0].fecha_inicio})
 
 def add_client_form(request):
     if request.method == "POST":
         form = AddClientForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             # THIS IS EXTREMELY REDUNDANT, WRITE IT BETTER LATTER
-            casa = form.cleaned_data['casa']
-            nombre = form.cleaned_data['nombre']
-            cantidad_personas = form.cleaned_data['cantidad_personas']
-            fecha_inicio = form.cleaned_data['fecha_inicio']
-            fecha_fin = form.cleaned_data['fecha_fin']
-            notas = form.cleaned_data['notas']
-
+            r = Reservas(casa=form.cleaned_data['casa'], nombre=form.cleaned_data['nombre'], cantidad_personas=form.cleaned_data['cantidad_personas'],
+                        fecha_inicio=form.cleaned_data['fecha_inicio'], fecha_fin=form.cleaned_data['fecha_fin'], notas=form.cleaned_data['notas'])
+            r.save()
             return render(request, 'calendarios/main.html')
 
     if request.method == "GET":
