b6268f123b80bc935742a9856e66d1dfefca093f
Mathiconroy <mathiconroy@gmail.com>
2020-01-03 17:50:48 -0300

    Added validation for dates, TODO: Logic for table at index.html

--- calendario_atyra_root/calendarios/forms.py
+++ calendario_atyra_root/calendarios/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from .models import Reservas
+
+from datetime import timedelta
+
+def validate_date(value):
+    pass
 
 class AddClientForm(forms.Form): # If let blank, they are REQUIRED by default
     # TODO: Add a validator for fecha_inicio and fecha_fin (There MUST NOT be a reservation in that house between the two dates!!!)
@@ -7,4 +13,16 @@ class AddClientForm(forms.Form): # If let blank, they are REQUIRED by default
     cantidad_personas = forms.IntegerField(label='Cantidad de personas', min_value=1, max_value=10, widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Cantidad de personas'}))
     fecha_inicio = forms.DateField(label='Fecha de inicio', widget=forms.DateInput(attrs={'class':'form-control', 'type':'date'}))
     fecha_fin = forms.DateField(label='Fecha de fin', widget=forms.DateInput(attrs={'class':'form-control', 'type':'date'}))
-    notas = forms.CharField(label='Notas', widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Notas'}))
+    notas = forms.CharField(required=False, label='Notas', widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Notas'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        fecha_inicio = cleaned_data.get('fecha_inicio')
+        fecha_fin = cleaned_data.get('fecha_fin')
+        casa = cleaned_data.get('casa')
+
+        if fecha_fin and fecha_inicio:
+            if not ((fecha_fin - fecha_inicio) >= timedelta(days=0)):
+                raise forms.ValidationError("La fecha final es antes de la fecha inicial, asegurese de no estar poniendolas al reves.")
+            elif Reservas.objects.filter(fecha_inicio__gte=fecha_inicio).filter(fecha_fin__lte=fecha_fin).filter(casa=casa):
+                raise forms.ValidationError("Esta fecha ya fue reservada para esta casa.")
--- calendario_atyra_root/calendarios/views.py
+++ calendario_atyra_root/calendarios/views.py
@@ -3,15 +3,17 @@ from django.http import HttpRequest, HttpResponse
 from .forms import AddClientForm
 from .models import Reservas
 
-def index(request):
-    reserva = Reservas.objects.filter()
-    return render(request, 'calendarios/main.html', {'nombre':reserva[0].fecha_inicio})
+from datetime import date, datetime, timedelta
+
+def index(request): # TODO: Populate the table (Having problems with the logic)
+    reserva = Reservas.objects.filter(fecha_inicio__lte=date.today() + timedelta(days=30))
+    return render(request, 'calendarios/main.html', {'reservas':reserva})
 
 def add_client_form(request):
     if request.method == "POST":
         form = AddClientForm(request.POST)
         if form.is_valid():
-            # THIS IS EXTREMELY REDUNDANT, WRITE IT BETTER LATTER
+            form = form.clean() # This is here to validate again with my custom clean() method in forms.py
             r = Reservas(casa=form.cleaned_data['casa'], nombre=form.cleaned_data['nombre'], cantidad_personas=form.cleaned_data['cantidad_personas'],
                         fecha_inicio=form.cleaned_data['fecha_inicio'], fecha_fin=form.cleaned_data['fecha_fin'], notas=form.cleaned_data['notas'])
             r.save()
@@ -19,4 +21,5 @@ def add_client_form(request):
 
     if request.method == "GET":
         form = AddClientForm()
-        return render(request, 'calendarios/form.html', {'form':form})
+
+    return render(request, 'calendarios/form.html', {'form':form})
