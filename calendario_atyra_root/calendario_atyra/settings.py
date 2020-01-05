455d4e0fd6d2ab0e73ca1b5122ddec0d79b57731
Mathiconroy <mathiconroy@gmail.com>
2020-01-05 13:23:21 -0400

    FUCKING HELP ME GOD

--- calendario_atyra_root/calendario_atyra/settings.py
+++ calendario_atyra_root/calendario_atyra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zq$&ay#cqx&b%e_n^_0u@nhcbor8a0ci2(r)dn6+ji(!$*0-e7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['calendario-atyra.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
