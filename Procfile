2c11da70ebee6beaec33e2bd9c60be8ce95afd29
Mathiconroy <mathiconroy@gmail.com>
2020-01-05 13:05:58 -0400

    WILL THIS TIME WORK? WHO KNOWS!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn calendario_atyra.wsgi --log-file -
--- calendario_atyra_root/calendario_atyra/settings.py
+++ calendario_atyra_root/calendario_atyra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zq$&ay#cqx&b%e_n^_0u@nhcbor8a0ci2(r)dn6+ji(!$*0-e7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['calendario-atyra.herokuapp.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +124,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'calendario_atyra/static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
