9b6d0c86c8ad181ce49ff838e831034f1206ecef
Mathiconroy <mathiconroy@gmail.com>
2020-01-06 09:29:09 -0300

    Update views.py

--- calendarios/views.py
+++ calendarios/views.py
@@ -37,8 +37,8 @@ def add_client_form(request):
         form = AddClientForm(request.POST)
         if form.is_valid():
             form = form.clean() # This is here to validate again with my custom clean() method in forms.py
-            r = Reservas(casa=form.cleaned_data['casa'], nombre=form.cleaned_data['nombre'], cantidad_personas=form.cleaned_data['cantidad_personas'],
-                        fecha_inicio=form.cleaned_data['fecha_inicio'], fecha_fin=form.cleaned_data['fecha_fin'], notas=form.cleaned_data['notas'])
+            r = Reservas(casa=form['casa'], nombre=form['nombre'], cantidad_personas=form['cantidad_personas'],
+                        fecha_inicio=form['fecha_inicio'], fecha_fin=form['fecha_fin'], notas=form['notas'])
             r.save()
             return redirect('index')
 
