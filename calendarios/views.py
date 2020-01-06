bd01be8766a01219dfc15a4375d8a34d03ce6cb9
Mathiconroy <mathiconroy@gmail.com>
2020-01-06 09:19:25 -0300

    Trying stuff with form.clean()

--- calendarios/views.py
+++ calendarios/views.py
@@ -36,7 +36,7 @@ def add_client_form(request):
     if request.method == "POST":
         form = AddClientForm(request.POST)
         if form.is_valid():
-            # form = form.clean() # This is here to validate again with my custom clean() method in forms.py
+            form = form.clean() # This is here to validate again with my custom clean() method in forms.py
             r = Reservas(casa=form.cleaned_data['casa'], nombre=form.cleaned_data['nombre'], cantidad_personas=form.cleaned_data['cantidad_personas'],
                         fecha_inicio=form.cleaned_data['fecha_inicio'], fecha_fin=form.cleaned_data['fecha_fin'], notas=form.cleaned_data['notas'])
             r.save()
