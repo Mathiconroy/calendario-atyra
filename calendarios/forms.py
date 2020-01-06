c4cdaf00da7ad3931b136f2ebbe8514340e1685c
Mathiconroy <mathiconroy@gmail.com>
2020-01-06 12:01:49 -0300

    Changed error text, TODO: Add a warning maybe?

--- calendarios/forms.py
+++ calendarios/forms.py
@@ -15,18 +15,23 @@ class AddClientForm(forms.Form): # If let blank, they are REQUIRED by default
     fecha_fin = forms.DateField(label='Fecha de fin', widget=forms.DateInput(attrs={'class':'form-control', 'type':'date'}))
     # Add email field?
     notas = forms.CharField(required=False, label='Notas', widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Notas'}))
+    warning = forms.BooleanField(required=False)
 
-    def clean(self):
+    def clean(self): # TODO: ADD A WARNING IF A RESERVATION IS MORE THAN X DAYS LONG, MOM SCARED THE SHIT OUT OF ME
         cleaned_data = super().clean()
         fecha_inicio = cleaned_data.get('fecha_inicio')
         fecha_fin = cleaned_data.get('fecha_fin')
         casa = cleaned_data.get('casa')
+        warning = cleaned_data.get('warning')
 
         if fecha_fin and fecha_inicio:
             if not ((fecha_fin - fecha_inicio) >= timedelta(days=0)):
-                raise forms.ValidationError("La fecha final es antes de la fecha inicial, asegurese de no estar poniendolas al reves.")
+                raise forms.ValidationError("ERROR: La fecha final es antes de la fecha inicial.")
             if Reservas.objects.filter(fecha_inicio__gte=fecha_inicio).filter(fecha_fin__lte=fecha_fin).filter(casa=casa):
-                raise forms.ValidationError("Esta fecha ya fue reservada para esta casa.")
-        
+                raise forms.ValidationError("ERROR: Esta fecha ya fue reservada para esta casa.")
+            if ((fecha_fin - fecha_inicio) >= timedelta(days=7)):
+                warning = False
+                self.add_error('fecha_fin', 'ADVERTENCIA: La reserva dura más de 7 dias. Checke la caja al final del formulario para confirmar los datos ingresados.')
+
         return cleaned_data
     
