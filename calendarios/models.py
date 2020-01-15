9e35d1c12a2abe7d6247f06258634863d0f20bab
Mathiconroy <mathiconroy@gmail.com>
2020-01-15 16:34:12 -0300

    testing

--- calendarios/models.py
+++ calendarios/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Reservas(models.Model):
-    id = models.IntegerField(primary_key=True, null=True)
     casa = models.IntegerField(null=False)
     nombre = models.CharField(null=False, max_length=150)
     email = models.EmailField(null=True)
