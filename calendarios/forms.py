9450d3397c5147ccfbcf3634442a8690d9b5e2a2
Mathiconroy <mathiconroy@gmail.com>
2020-01-17 09:40:12 -0300

    Fixed the email thing!

--- calendarios/forms.py
+++ calendarios/forms.py
@@ -6,7 +6,7 @@ from django.db.models import F
 from datetime import timedelta
 
 class AddClientForm(forms.Form): # If let blank, they are REQUIRED by DEFAULT
-    id = forms.IntegerField(required=False, label='ID')
+    id = forms.IntegerField(required=False, label='ID', widget=forms.HiddenInput())
     casa = forms.ChoiceField(label='Casa', choices=[('', 'Seleccione una casa'), (1, 'Barro Roga'), (2, 'Ysypo Roga'), (3, 'Hierro Roga')], widget=forms.Select(attrs={'class':'form-control'}))
     nombre = forms.CharField(label='Nombre', max_length=150, strip=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre del cliente'}))
     email = forms.EmailField(required=False, label='Email', widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email del cliente'}))
@@ -15,7 +15,7 @@ class AddClientForm(forms.Form): # If let blank, they are REQUIRED by DEFAULT
     fecha_fin = forms.DateField(label='Fecha de fin', widget=forms.DateInput(attrs={'class':'form-control', 'type':'date'}))
     cantidad_dias = forms.IntegerField(label='Cantidad de dias', min_value=0, widget=forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Cantidad de dias'}))
     notas = forms.CharField(required=False, label='Notas', widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Notas'}))
-    edit = forms.BooleanField(label='Editar', required=False) # TODO: ADD THE HIDDENINPUT WIDGET!!!
+    edit = forms.BooleanField(label='Editar', required=False, widget=forms.HiddenInput()) # TODO: ADD THE HIDDENINPUT WIDGET!!!
 
     # NOTE TO SELF: PYTHON EVALUATES 0 TO FALSE AND OTHER NUMBERS TO TRUE LMAOOOOOOOOOOOO
     def clean(self):
@@ -27,7 +27,7 @@ class AddClientForm(forms.Form): # If let blank, they are REQUIRED by DEFAULT
         edit = cleaned_data.get('edit')
         id = cleaned_data.get('id')
 
-        if (fecha_fin and fecha_inicio and cantidad_dias) or fecha_fin and fecha_inicio and cantidad_dias == 0:
+        if (fecha_fin and fecha_inicio and cantidad_dias) or (fecha_fin and fecha_inicio and cantidad_dias == 0):
             if not ((fecha_fin - fecha_inicio) >= timedelta(days=0)):
                 raise forms.ValidationError("ERROR: La fecha final es antes de la fecha inicial.")
             if not ((fecha_fin - fecha_inicio) == timedelta(days=cantidad_dias)):
@@ -35,16 +35,14 @@ class AddClientForm(forms.Form): # If let blank, they are REQUIRED by DEFAULT
             if casa == '':
                 self.add_error('casa', 'ERROR: La casa seleccionada no es válida.')
             if not edit:
-                if Reservas.objects.filter(fecha_inicio__gte=fecha_inicio).filter(fecha_fin__lte=fecha_fin).filter(casa=int(casa)):
+                if (Reservas.objects.filter(fecha_inicio__range=(fecha_inicio, fecha_fin)).filter(casa=int(casa)) or
+                    Reservas.objects.filter(fecha_fin__range=(fecha_inicio, fecha_fin)).filter(casa=int(casa)) or 
+                    Reservas.objects.filter(fecha_inicio__gte=fecha_inicio).filter(fecha_fin__lte=fecha_fin).filter(casa=int(casa))):
                     raise forms.ValidationError("ERROR: Esta fecha ya fue reservada para esta casa.")
             else:
-                r = Reservas.objects.filter(fecha_inicio__gte=fecha_inicio).filter(fecha_fin__lte=fecha_fin).filter(casa=int(casa)).exclude(id=id)
-                # TODO: Fix this, for some reason it returns expected results if fecha_fin + 1
-                # LOL THIS ALGORITHM IS WRONG FUCK
-                print(fecha_inicio)
-                print(fecha_fin)
-                print(r)
-                if r:
+                if (Reservas.objects.filter(fecha_inicio__range=(fecha_inicio, fecha_fin)).filter(casa=int(casa)).exclude(id=id) or
+                    Reservas.objects.filter(fecha_fin__range=(fecha_inicio, fecha_fin)).filter(casa=int(casa)).exclude(id=id) or 
+                    Reservas.objects.filter(fecha_inicio__gte=fecha_inicio).filter(fecha_fin__lte=fecha_fin).filter(casa=int(casa)).exclude(id=id)):
                     raise forms.ValidationError("ERROR: Esta fecha ya fue reservada para esta casa.")
 
         return cleaned_data
