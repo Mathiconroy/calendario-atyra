e181e0d9af1cca96723f4c1d6598bdbe8d7976b6
Mathiconroy <mathiconroy@gmail.com>
2020-01-23 11:43:09 -0300

    Deleted a comment

--- calendario_atyra/settings.py
+++ calendario_atyra/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LOGIN_URL = '/login'
 
-LOGIN_REDIRECT_URL = '/' # TODO: Ask mom cookie related questions (do I want the session to expire if the browser closses?)
+LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/login'
 
