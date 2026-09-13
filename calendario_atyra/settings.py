06fe1913a99ada7f41e938a0837008b49fb2d8e0
Mathiconroy <mathiconroy@gmail.com>
2026-09-13 17:44:12 -0300

    Update settings again

--- calendario_atyra/settings.py
+++ calendario_atyra/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", "SHITTY-DEV-KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG_MODE", True))
+DEBUG = bool(int(os.environ.get("DEBUG_MODE", 0)))
 
 ALLOWED_HOSTS = [
     os.environ.get("HOST", "127.0.0.1"),
